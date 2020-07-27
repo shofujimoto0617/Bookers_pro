96aa85b4c3f58a0d7634babe7346d89f2862ec04
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-27 20:31:23 +0900

    [add]編集画面設置

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,11 +12,18 @@ class BooksController < ApplicationController
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
 
   def create
   	@book = Book.new(book_params)
   	@book.save
+  	redirect_to book_path(@book.id)
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	@book.update(book_params)
   	redirect_to book_path(@book.id)
   end
 
