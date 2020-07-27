1278aa14a62da688fe3a34aae70544018502ee56
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-27 22:09:07 +0900

    [Update]新規投稿画面を一覧画面に移動

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,11 @@
 class BooksController < ApplicationController
   def index
   	@books = Book.all
+  	@book = Book.new
   end
 
   def show
   	@book = Book.find(params[:id])
-  end
-
-  def new
-  	@book = Book.new
   end
 
   def edit
