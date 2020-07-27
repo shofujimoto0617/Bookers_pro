349afbcc7f2799c98490d5b8f1f439f0f340be81
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-27 19:49:50 +0900

    [add]詳細画面設置

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   end
 
   def show
-  	@book = Book.find(book_params)
+  	@book = Book.find(params[:id])
   end
 
   def new
@@ -14,8 +14,8 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
+  	@book = Book.new(book_params)
+  	@book.save
   	redirect_to book_path(@book.id)
   end
 
