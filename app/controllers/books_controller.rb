59e9733c14722abe95d3e51040a292056ca6193d
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-27 19:33:15 +0900

    [add]新規投稿画面設置

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  	@book = Book.find(book_params)
+  end
+
+  def new
+  	@book = Book.new
+  end
+
+  def edit
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to book_path(@book.id)
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
