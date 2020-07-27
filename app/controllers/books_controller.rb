7bebb4145457b7c5d7461fbf8d2e24003128dbe2
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-27 22:36:06 +0900

    [add]renderエラー表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,8 +14,13 @@ class BooksController < ApplicationController
 
   def create
   	@book = Book.new(book_params)
-  	@book.save
-  	redirect_to book_path(@book.id)
+  	@books = Book.all
+  	if @book.save
+  	  flash[:notice] = "Book was successfully created."
+  	  redirect_to book_path(@book.id)
+  	else
+  	  render :index
+  	end
   end
 
   def update
